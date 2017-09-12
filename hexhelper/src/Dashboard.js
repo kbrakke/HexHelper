43be5e547fb126b0602f308db0612fc38cc66cc7
kbrakke <kbrakke@gmail.com>
2017-09-12 13:22:35 -0400

    Add bootstrap, initial header

--- hexhelper/src/Dashboard.js
+++ hexhelper/src/Dashboard.js
@@ -4,12 +4,22 @@ import HexHeader from './Hex/HexHeader.js'
 class Dashboard extends Component {
   constructor (props) {
     super(props)
-    this.state = {selectedHex: null}
+    this.state = {selectedHex: {label: 'H3',
+      terrain: {label: 'Forest (dense)',
+        highwayMultiplier: 1,
+        roadMultiplier: 1,
+        tracklessMultiplier: 0.5,
+        navDC: 18,
+        forageDC: 14
+      }}}
   }
   render () {
     return (
       <div className="dashboard">
-        <HexHeader hex={this.state.selectedHex} />
+        <HexHeader selectedHex={this.state.selectedHex} />
+        <div>
+
+        </div>
       </div>
     )
   }
--- hexhelper/src/Hex/HexHeader.js
+++ hexhelper/src/Hex/HexHeader.js
@@ -1,10 +1,25 @@
 import React, { Component } from 'react'
+import './HexHeader.css'
 
 class HexHeader extends Component {
   render () {
-    return <div class="hex-header">
-      {this.props.selectedHex.label}
-    </div>
+    return <header>
+      <h2>
+        {this.props.selectedHex.label}
+      </h2>
+      <img src={'/forestHex.png'} alt={'Forest Icon'} height={'18'} width={'16'} className="terrain-icon"/>
+      <h2>{this.props.selectedHex.terrain.label}</h2>
+      <table className={'terrain-stats'}>
+        <tr>
+          <td><strong>Highway: x{this.props.selectedHex.terrain.highwayMultiplier}</strong></td>
+          <td><strong>Road/Trail: x{this.props.selectedHex.terrain.roadMultiplier}</strong></td>
+          <td><strong>Road/Trail: x{this.props.selectedHex.terrain.roadMultiplier}</strong></td>
+          <td><strong>Trackless: x{this.props.selectedHex.terrain.tracklessMultiplier}</strong></td>
+          <td><strong>Navigation: DC {this.props.selectedHex.terrain.navDC}</strong></td>
+          <td><strong>Forage DC: {this.props.selectedHex.terrain.forageDC}</strong></td>
+        </tr>
+      </table>
+    </header>
   }
 }
 
